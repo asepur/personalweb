333542dae9fc7e0de73eb2014a766d2fc0690270
asepur <asepur94@gmail.com>
2025-03-04 16:16:51 +0100

    Añadidos dos proyectos

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -17,32 +17,46 @@ const projectsData = [
         image: "https://i.ibb.co/51t08MJ/abstract.jpg",
         url: "https://abstract-asepur.netlify.app/"
     },
+
     {
         id: 4,
+        tags: ["UX/UI", "Figma"],
+        image: "https://i.ibb.co/G4s7Tsff/Headphones-Product-Page.jpg",
+        url: "https://www.figma.com/proto/M0IdS5x4GH5nR31VOU8RR4/Sesiones-UI?node-id=203-2&p=f&t=SFMZJPZbvQkY15xp-0&scaling=min-zoom&content-scaling=fixed&page-id=1%3A4&starting-point-node-id=203%3A2"
+    },
+
+    {
+        id: 5,
+        tags: ["UX/UI", "Figma"],
+        image: "https://i.ibb.co/NnZK60mh/Wireframe-1.jpg",
+        url: "https://www.figma.com/design/QUMQozY8uHCbNvDu6ESc3k/UX%2FUI-Excercise?node-id=0-1&p=f&t=Mq6x7BWGCsLN9Jtz-0"
+    },
+    {
+        id: 6,
         tags: ["UX/UI", "Figma"],
         image: "https://i.ibb.co/NnX8FpD/renault.jpg",
         url: "https://www.figma.com/proto/sdZNQLREWnimbsJHoZh94m/ORGANIC?node-id=447-3&node-type=frame&t=wZldyj5QoVQeS4KQ-0&scaling=min-zoom&content-scaling=fixed&page-id=447%3A2"
     },
     {
-        id: 5,
+        id: 7,
         tags: ["Figma", "UX/UI"],
         image: "https://i.ibb.co/PNc7C6B/tomsent.webp",
         url: "https://www.figma.com/proto/L9eeBr5ImruAxz4YKNGVrD/Untitled?node-id=4-96&t=vIzhLOGKr01ANPyO-1"
     },
     {
-        id: 6,
+        id: 8,
         tags: ["Illustrator"],
         image: "https://i.ibb.co/44SfkZL/coven.webp",
         url: "https://www.instagram.com/p/BoG1KwmjvpE/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
     },
     {
-        id: 7,
+        id: 9,
         tags: ["Photoshop", "Illustrator", "InDesign"],
         image: "https://i.ibb.co/SsQ0jyh/invitaciones1.webp",
         url: "#"
     },
     {
-        id: 8,
+        id: 10,
         tags: ["Photoshop", "Illustrator"],
         image: "https://i.ibb.co/rGdHH7z/invitaciones2.webp",
         url: "https://www.instagram.com/p/CuhpmoTsldv/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
