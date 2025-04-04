f2985f4710a0adf96c52f75253b77cc300a41ee8
asepur <asepur94@gmail.com>
2025-04-04 12:45:59 +0200

    Actualización Nombre Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,11 @@ function Hero() {
 	return (
 
         <section className="hero__container">
-          <img src="https://i.ibb.co/RHRFzfj/silhouette.png" alt="hero-img" className='hero__container--img'/>
+          <img src="https://i.ibb.co/RHRFzfj/silhouette.png" alt="Ilustración original Samurai I" className='hero__container--img'/>
 
           <div className='hero__container--name'>
-            <span>Andrés</span>
-            <span>Sepúlveda</span>
-            <span>Ruiz</span>
+            <span>ASEPUR</span>
+            <span>dsign</span>
           </div>
       </section>
     );
