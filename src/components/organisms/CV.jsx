797522f13fd37d742c738a582d2d3b9db75ef342
asepur <asepur94@gmail.com>
2025-07-22 12:06:05 +0200

    Actualización 2.6 cv

--- src/components/organisms/CV.jsx
+++ src/components/organisms/CV.jsx
@@ -1,154 +1,217 @@
 import './cv.css'
 
-
 function Cv() {
-	return (
-        <div className='cv' >
-          <h2>Experiencia Laboral</h2>
+  return (
+    <section className="cv">
+      <h2>Experiencia Laboral</h2>
 
-          <div className='cv__cargo'>
-            <h3>Diseñador Gráfico Senior - 360</h3>
-
-            <div className='cv__cargo--info'>
-              <div className='cv__cargo--resume'>
-                <span>RegalosPublicitarios.com</span>
-                <span>Promotionice.com</span>
-              </div>
-
-              <div className='cv__cargo--resume'>
-                  <span>2018 - Actualmente</span>
-              </div>
-
-            </div>
-            
-            <div>
-              <h4>Habilidades requeridas</h4>
-              <div className='cv__cargo--skills'>
-                <span>Liderazgo de equipo</span>
-                <span>Photoshop</span>
-                <span>Illustrator</span>
-                <span>HTML</span>
-                <span>CSS</span>
-                <span>Javascript</span>
-                <span>Figma</span>
-                <span>Microsoft Clarity</span>
-                <span>GA 4</span>
-                <span>Elementor</span>
-              </div>
-              
-            </div>
-            <div>
-              <h4>Principales responsabilidades</h4>
-              
-              <ul>
-                <li>
-                  <p><strong>Diseño Gráfico</strong><br/> Diseño de mockups y piezas visuales para e-commerce, <strong>aumentando la producción diaria en un 87%.</strong> Desarrollo de materiales de branding para fortalecer la identidad visual de la marca.
-                  </p>
-                </li>
-                <li>
-                  <p><strong>UX</strong><br/> 
-                  Análisis de comportamiento del usuario con Clarity para evaluar mejoras en la web. Enfoque en métricas como permanencia y conversión.
-                  </p>
-                </li>
-                <li>
-                  <p><strong>UI</strong><br/>
-                  Diseño y rediseño de interfaces: wireframes, prototipos interactivos y MVP para nuevas funcionalidades.                  </p>
-                </li>
-                <li>
-                  <p><strong>Liderazgo y estructura de equipo</strong><br/> 
-                  Redefinición del rol del área de diseño. Implementé procesos, documentación y KPIs, duplicando la productividad y ampliando responsabilidades estratégicas.
-                  </p>
-                </li>
-                <li>
-                  <p>
-                  <strong>Colaboración interdepartamental</strong><br/>
-                  con desarrollo para la implementación de mejoras en la web 
-                  y con marketing en el diseño de campañas visuales, asegurando una comunicación fluida entre equipos 
-                  y una ejecución eficiente de los proyectos.
-                  </p>
-                </li>
-                <li>
-                  <p>
-                  <strong>Project Management</strong><br/>
-                  Coordinación del equipo de diseño, formación de becarios y gestión de tareas diarias.
-                  </p>
-                </li>
-                
-              </ul>
-            </div>
-            
+      <article className="cv__job">
+        <header className="cv__job--header">
+          <h3>Diseñador Gráfico Senior - 360</h3>
+          <div className="cv__job--meta">
+            <span>RegalosPublicitarios.com | Promotionice.com</span>
+            <span> - </span>
+            <span>2018 - Actual</span>
           </div>
-          <br />
-          <br />
-          <div className='cv__cargo'>
-            <h3>Diseñador Gráfico Freelance</h3>
-            <div className='cv__work--case'>
-              <div className='cv__cargo--resume'>
-                <h4>Logo Filada Ramón Muntaner</h4>
-                <p>2025</p>
-              </div>
-              <div className='cv__cargo--skills'>
-                <span>Illustrator</span>
-              </div>
-            </div>
-           
-            <div className='cv__work--case'>
-              <div className='cv__cargo--resume'>
-                <h4>Invitaciones de Boda Calas de Alicante</h4>
-                <p>2024</p>
-              </div>
-              <div className='cv__cargo--skills'>
-                <span>InDesign</span>
-                <span>Photoshop</span>
-                <span>Illustrator</span>
-                <span>Impresión Offset</span>
-              </div>
-            </div>
+        </header>
 
-            <div className='cv__work--case'>
-              <div className='cv__cargo--resume'>
-                <h4>Invitaciones de Boda Harry Potter</h4>
-                <p>2023</p>
-              </div>
-              <div className='cv__cargo--skills'>
-                <span>InDesign</span>
-                <span>Photoshop</span>
-                <span>Illustrator</span>
-                <span>Impresion Offset</span>
-              </div>
-            </div>
-
-            <div className='cv__work--case'>
-              <div className='cv__cargo--resume'>
-                <h4>Logotipo La Tintas Tattoo</h4>
-                <p>2023</p>
-              </div>
-                <div className='cv__cargo--skills'>
-                    <span>Illustrator</span>
-                    <span>Procreate</span>
-                </div>
-            </div>
-
-            <div className='cv__work--case'>
-                <div className='cv__cargo--resume'>
-                    <h4>Tarjetas de visita - COVEN Restaurant</h4>
-                    <p>2019</p>
-                </div>
-                <div className='cv__cargo--skills'>
-                    <span>Illustrator</span>
-                    <span>Impresion Offset</span>
-                </div>
-            </div>
-
+        <section className="cv__section">
+          <h4>Skills</h4>
+          <div className="cv__tags">
+            <span>Liderazgo de equipo</span>
+            <span>Creación de contenido</span>
+            <span>Formación de becarios</span>
+            <span>Figma</span>
+            <span>Microsoft Clarity</span>
+            <span>User Research</span>
+            <span>Screen Record</span>
+            <span>GA 4</span>
+            <span>HTML</span>
+            <span>CSS</span>
+            <span>Javascript</span>
+            <span>CMS</span>
+            <span>Heat Maps</span>
+            <span>Photoshop</span>
+            <span>CorelDraw</span>
           </div>
+        </section>
 
-          <div>
+        <section className="cv__section">
+          <h4>Principales responsabilidades</h4>
+          <ul className="cv__responsibilities">
+            <li>
+              <p><strong>Diseño Gráfico</strong><br />
+                Diseño y creación de contenido web y mockups para e-commerce, <strong>aumentando la producción diaria en un 87%</strong>. Desarrollo de materiales de branding para fortalecer la identidad visual de la marca.
+              </p>
+            </li>
+            <li>
+              <p><strong>UX</strong><br />
+                Screen records, heatmaps, shadowing o entrevistas para evaluar mejoras en la web. Análisis de datos y creación de métricas permanencia y conversión.
+              </p>
+            </li>
+            <li>
+              <p><strong>UI</strong><br />
+                Diseño y rediseño de interfaces: wireframes, prototipos interactivos y MVP para nuevas funcionalidades.
+              </p>
+            </li>
+            <li>
+              <p><strong>Liderazgo y estructura de equipo</strong><br />
+                Redefinición del rol del área de diseño. Implementé procesos, documentación y KPIs, duplicando la productividad y ampliando responsabilidades estratégicas.
+              </p>
+            </li>
+            <li>
+              <p><strong>Colaboración interdepartamental</strong><br />
+                Con desarrollo para la implementación de mejoras en la web y con marketing en el diseño de campañas visuales, asegurando una comunicación fluida entre equipos y una ejecución eficiente de los proyectos.
+              </p>
+            </li>
+            <li>
+              <p><strong>Project Management</strong><br />
+                Coordinación del equipo de diseño, formación de becarios y gestión de tareas diarias.
+              </p>
+            </li>
+          </ul>
+        </section>
+      </article>
 
-            
+        <div className="cv__job">
+          <header className="cv__job--header">
+            <h3>Diseño de logotipo</h3>
+            <div className="cv__job--meta">
+              <span>Filada Ramón Muntaner</span>
+              <span> - </span>
+              <span>2024</span>
           </div>
-
+          </header>
+          <section className="cv__section">
+            <h4>Skills</h4>
+            <div className="cv__tags">
+              <span>Benchmarking</span>
+              <span>Illustrator</span>
+              <span>Impresión DTF</span>
+            </div>
+          </section>
+          <section className="cv__section">
+            <h4>Encargo</h4>
+            <div className="cv__responsibilities">
+                <p>Diseño de logotipo e impresión DTF en camisetas personalizadas.
+                </p>
+            </div>
+          </section>
         </div>
-    )
-}    
+
+
+        <div className="cv__job">
+          <header className="cv__job--header">
+            <h3>Invitaciones de Boda Calas de Alicante</h3>
+            <div className="cv__job--meta">
+              <span>Particulares</span>
+              <span> - </span>
+              <span>2023</span>
+          </div>
+          </header>
+          <section className="cv__section">
+            <h4>Skills</h4>
+            <div className="cv__tags">
+              <span>InDesign</span>
+              <span>Photoshop</span>
+              <span>Illustrator</span>
+              <span>Impresión Offset</span>
+              <span>Impresión DTF</span>
+              <span>Ilustración digital</span>
+            </div>
+          </section>
+          <section className="cv__section">
+            <h4>Encargo</h4>
+            <div className="cv__responsibilities">
+           
+                <p>Diseño de las invitaciones de boda, sitting plan, save the date, regalos personaalizados e ilustraciones de regalo.
+                </p>
+            </div>
+          </section>
+          
+        </div>
+
+        <div className="cv__job">
+          <header className="cv__job--header">
+            <h3>Invitaciones de Boda Harry Potter</h3>
+            <div className="cv__job--meta">
+              <span>Particulares</span>
+              <span> - </span>
+              <span>2022</span>
+          </div>
+          </header>
+          <section className="cv__section">
+            <h4>Skills</h4>
+            <div className="cv__tags">
+              <span>Photoshop</span>
+              <span>Illustrator</span>
+              <span>Impresión Offset</span>
+            </div>
+          </section>
+          <section className="cv__section">
+            <h4>Encargo</h4>
+            <div className="cv__responsibilities">
+                <p>Diseño de las invitaciones de boda inspirado en Harry Potter con ilustración personalizada.
+                </p>
+            </div>
+          </section>
+        </div>
+
+
+        <div className="cv__job">
+          <header className="cv__job--header">
+            <h3>Diseño de Logotipo</h3>
+            <div className="cv__job--meta">
+              <span>LaTintas Tattoo</span>
+              <span> - </span>
+              <span>2022</span>
+          </div>
+          </header>
+          <section className="cv__section">
+            <h4>Skills</h4>
+            <div className="cv__tags">
+              <span>Illustrator</span>
+              <span>Procreate</span>
+            </div>
+          </section>
+
+          <section className="cv__section">
+            <h4>Encargo</h4>
+            <div className="cv__responsibilities">
+                <p>Diseño de logotipo e identidad visual para el perfil digital de la artista de tatuaje LaTintas Tattoo.
+                </p>
+            </div>
+          </section>
+        </div>
+
+
+        <div className="cv__job">
+          <header className="cv__job--header">
+            <h3>Diseño de logotipo</h3>
+            <div className="cv__job--meta">
+              <span>Coven Restaurant</span>
+              <span> - </span>
+              <span>2022</span>
+          </div>
+          </header>
+          <section className="cv__section">
+            <h4>Skills</h4>
+            <div className="cv__tags">
+              <span>Illustrator</span>
+              <span>Impresión Offset</span>
+            </div>
+          </section>
+          <section className="cv__section">
+            <h4>Encargo</h4>
+            <div className="cv__responsibilities">
+                <p>Diseño del logotipo y tarjetas de visita para el restaurante Coven en Sagunto.
+                </p>
+            </div>
+          </section>
+        </div>
+
+    </section>
+  )
+}
 
 export default Cv
