332f1ac1904f61e681d2305cd1887a8ef402a7c8
asepur <asepur94@gmail.com>
2025-09-18 09:45:02 +0200

    Actualización 2.7.8 hero y estilos varios

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,7 @@ function Hero() {
         <section className="hero__container">
           <img src="https://i.ibb.co/n808qPkZ/grulla-blanca.png" alt="Ilustración original Samurai I: se muestra la grulla vestida de samurai señalando con la flecha hacia abajo." className='hero__container--img'/>
 
-          <div className='hero__container--name'>
-            <span>ASEPUR</span>
-            <span>dsign</span>
-          </div>
+          <h1>ASEPUR DESIGN</h1>
           
       </section>
     );
